51a6d7447b1bb6cf24ac4d100c9ab721ad2fd339
Jowi20 <joelorban15@gmail.com>
2022-06-11 18:57:50 -0300

    excelente perro, test unitario aqui

---
+++ test_calc.py
@@ -0,0 +1,10 @@
+from app import Calculator
+def test_add():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.add() == x + y, "La suma tiene problemas"
+
+def test_subtract():
+    x,y = 1,2
+    instance = Calculator(x,y)
+    assert instance.subtract() == x-y, "La resta no funciona bien"
