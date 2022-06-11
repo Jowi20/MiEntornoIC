209a1991dac7b03b7d22acf0b323a2eccb5229c2
Jowi20 <joelorban15@gmail.com>
2022-06-10 22:23:56 -0300

    app ya codeada

--- myapp.py
+++ myapp.py
@@ -1,1 +1,31 @@
-write("hola mundo")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv
+import bottle
+from bottle import route,run
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num1 - self.num2
+
+bottle.debug(True)
+
+@route('/')
+def index():
+	return "<h1>Hola Mundo, esto sera una calculadora web!!!</h1>"
+
+
+#if __name__ == '__main__':
+#	run(host='127.0.0.1',port=8000)
+
+
+if __name__ == '__main__':
+	run(host='0.0.0.0', port=argv[1])
