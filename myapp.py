e53d82ce69ac1041504c690cd9038d7262a9fa10
Jowi20 <joelorban15@gmail.com>
2022-06-11 22:12:03 -0300

    arregle la suma

--- myapp.py
+++ myapp.py
@@ -11,7 +11,7 @@ class Calculator:
         self.num2 = num2
 
     def add(self):
-        return self.num1 + self.num2 +1
+        return self.num1 + self.num2
 
     def subtract(self):
         return self.num1 - self.num2
